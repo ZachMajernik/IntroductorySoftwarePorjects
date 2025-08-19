755361578405d314c084aafbc62affeb0e7602dd
Zach Majernik <zachmajernik@gmail.com>
2025-08-19 17:43:10 -0600

    fixed up html

--- NotesAppExtention/scripts/popup.js
+++ NotesAppExtention/scripts/popup.js
@@ -1,3 +1,3 @@
 document.getElementById("openPage").addEventListener("click", () => {
-  chrome.tabs.create({ url: chrome.runtime.getURL("Notes_page.html") });
+  chrome.tabs.create({ url: chrome.runtime.getURL("login.html") });
 });
