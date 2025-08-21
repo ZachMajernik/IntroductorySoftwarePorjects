3cd2483e47f04c0ea5a783450867b5f2992e6f03
Zach Majernik <zachmajernik@gmail.com>
2025-08-20 19:47:27 -0600

    finished all js for login pages

--- NotesAppExtention/scripts/login_pages_script.js
+++ NotesAppExtention/scripts/login_pages_script.js
@@ -2,7 +2,9 @@
  * Storage layout
  * 
  * allAccountsInfo{
- * 'User1': {'password': password, 'securityInfo': {securityInfoDictionary}, 'notesData': {notesDataDictionary}}}
+ * 'User1': {'password': password, 
+ *           'securityInfo': {securityInfoDictionary}, 
+ *           'notesData': {notesDataDictionary}}}
  */
 
 let allAccountsInfo = {};
@@ -105,5 +107,55 @@ if (document.body.id === "login_page") {
         window.location.replace("notes.html");
     });
 } else if (document.body.id === "forgot_password_page") {
-    
+    const usernameInput = document.getElementById("username_input");
+    const invalidUsernameText = document.getElementById("invalid_username_text");
+    const getSecQueButton = document.getElementById("get_sec_que_button");
+
+    const  secQue1 = document.getElementById("sec_que_1");
+    const  secQue2 = document.getElementById("sec_que_2");
+    const  secQue3 = document.getElementById("sec_que_3");
+
+    const  answer1Input = document.getElementById("answer_1_input");
+    const  answer2Input = document.getElementById("answer_2_input");
+    const  answer3Input = document.getElementById("answer_3_input");
+
+    const getPasswordOutput = document.getElementById("get_password");
+    const getPasswordButton = document.getElementById("get_password_button");
+    const invalidAnswersText = document.getElementById("invalid_answers_text");
+
+    let username = "";
+
+    getSecQueButton.addEventListener("click", () => {
+        invalidUsernameText.innerHTML = "";
+        username = usernameInput.value;
+
+        if(!allAccountsInfo[username]){
+            invalidUsernameText.innerHTML = "<p>This username does not match one in our database</p>"
+            return;
+        }
+        usernameInput.disabled = true;
+        getSecQueButton.disabled = true;
+
+        secQue1.innerHTML = allAccountsInfo[username]['SecurityInfo']['SecQue1'][0]
+        secQue2.innerHTML = allAccountsInfo[username]['SecurityInfo']['SecQue2'][0]
+        secQue3.innerHTML = allAccountsInfo[username]['SecurityInfo']['SecQue3'][0]
+    });
+
+    getPasswordButton.addEventListener("click", () => {
+        invalidAnswersText.innerHTML = "";
+
+        console.log(answer1Input.value + " " + allAccountsInfo[username]['SecurityInfo']['SecQue1'][1])
+        console.log(answer2Input.value + " " + allAccountsInfo[username]['SecurityInfo']['SecQue2'][1])
+        console.log(answer3Input.value + " " + allAccountsInfo[username]['SecurityInfo']['SecQue3'][1])
+
+        if(!((answer1Input.value === allAccountsInfo[username]['SecurityInfo']['SecQue1'][1]) &&
+            (answer2Input.value === allAccountsInfo[username]['SecurityInfo']['SecQue2'][1]) && 
+            (answer3Input.value === allAccountsInfo[username]['SecurityInfo']['SecQue3'][1]))){
+
+            invalidAnswersText.innerHTML = "<p>One or more answers are incorrect</p>";
+            return;
+        }
+
+        getPasswordOutput.innerHTML = `<h3>Your Password:</h3><p>${allAccountsInfo[username]['Password']}</p>`
+    });
 }
